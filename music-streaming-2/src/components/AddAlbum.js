93d0f327b61f2a5efecadd4446de5415a4adcf9d
saiteja6 <bendalamsaiteja6@gmail.com>
2021-03-09 19:28:17 +0530

    added input fields, event listeners and submit button

--- music-streaming-2/src/components/AddAlbum.js
+++ music-streaming-2/src/components/AddAlbum.js
@@ -4,13 +4,28 @@ export default class AddAlbum extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            album_title: '',
+            artist: ''    
         }
     }
     render() {
         return (
             <div>
-                
+                <form>
+                    <input
+                        type='text'
+                        value={this.state.album_title}
+                        onChange={this.handleChange}
+                        placeholder='Enter the name of album title'
+                    ></input>
+                    <input
+                        type='text'
+                        value={this.state.artist}
+                        onChange={this.handleChange}
+                        placeholder='Enter the name of artist'
+                    ></input>
+                    <button type='submit'>Submit</button>
+                    </form>    
             </div>
         )
     }
